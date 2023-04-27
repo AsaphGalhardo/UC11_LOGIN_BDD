93393af84fd37dfef1f20f6066d53e13fa30920c
AsaphGalhardo <asaphgalhardo@gmail.com>
2023-04-26 21:07:11 -0300

    Adicionar arquivos de projeto.

---
+++ UC11_LOGIN_BDD/Form.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace UC11_LOGIN_BDD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxUSUARIO = new System.Windows.Forms.TextBox();
+            this.textBoxSENHA = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonENTRAR = new System.Windows.Forms.Button();
+            this.buttonLIMPAR = new System.Windows.Forms.Button();
+            this.buttonCADASTRAR = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxUSUARIO
+            // 
+            this.textBoxUSUARIO.Location = new System.Drawing.Point(85, 69);
+            this.textBoxUSUARIO.Name = "textBoxUSUARIO";
+            this.textBoxUSUARIO.Size = new System.Drawing.Size(144, 20);
+            this.textBoxUSUARIO.TabIndex = 0;
+            // 
+            // textBoxSENHA
+            // 
+            this.textBoxSENHA.Location = new System.Drawing.Point(85, 141);
+            this.textBoxSENHA.Name = "textBoxSENHA";
+            this.textBoxSENHA.Size = new System.Drawing.Size(144, 20);
+            this.textBoxSENHA.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Usuário";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Senha";
+            // 
+            // buttonENTRAR
+            // 
+            this.buttonENTRAR.Location = new System.Drawing.Point(65, 192);
+            this.buttonENTRAR.Name = "buttonENTRAR";
+            this.buttonENTRAR.Size = new System.Drawing.Size(75, 23);
+            this.buttonENTRAR.TabIndex = 4;
+            this.buttonENTRAR.Text = "Entrar";
+            this.buttonENTRAR.UseVisualStyleBackColor = true;
+            this.buttonENTRAR.Click += new System.EventHandler(this.buttonENTRAR_Click);
+            // 
+            // buttonLIMPAR
+            // 
+            this.buttonLIMPAR.Location = new System.Drawing.Point(181, 192);
+            this.buttonLIMPAR.Name = "buttonLIMPAR";
+            this.buttonLIMPAR.Size = new System.Drawing.Size(75, 23);
+            this.buttonLIMPAR.TabIndex = 5;
+            this.buttonLIMPAR.Text = "Limpar";
+            this.buttonLIMPAR.UseVisualStyleBackColor = true;
+            // 
+            // buttonCADASTRAR
+            // 
+            this.buttonCADASTRAR.Location = new System.Drawing.Point(114, 246);
+            this.buttonCADASTRAR.Name = "buttonCADASTRAR";
+            this.buttonCADASTRAR.Size = new System.Drawing.Size(75, 23);
+            this.buttonCADASTRAR.TabIndex = 6;
+            this.buttonCADASTRAR.Text = "Cadastrar";
+            this.buttonCADASTRAR.UseVisualStyleBackColor = true;
+            this.buttonCADASTRAR.Click += new System.EventHandler(this.buttonCADASTRAR_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 319);
+            this.Controls.Add(this.buttonCADASTRAR);
+            this.Controls.Add(this.buttonLIMPAR);
+            this.Controls.Add(this.buttonENTRAR);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxSENHA);
+            this.Controls.Add(this.textBoxUSUARIO);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxUSUARIO;
+        private System.Windows.Forms.TextBox textBoxSENHA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonENTRAR;
+        private System.Windows.Forms.Button buttonLIMPAR;
+        private System.Windows.Forms.Button buttonCADASTRAR;
+    }
+}
+
---
+++ UC11_LOGIN_BDD/Form.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace UC11_LOGIN_BDD
+{
+    public partial class Form1 : Form
+    {
+        string servidor;
+        MySqlConnection conexao;
+        MySqlCommand comando; 
+
+
+        public Form1()
+        {
+            InitializeComponent();
+             servidor = "Server=localhost;Database=login_bdd;Uid=root;Pwd=";
+            conexao = new MySqlConnection(servidor);
+             comando = conexao.CreateCommand();
+        }
+
+        private void buttonENTRAR_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                conexao.Open();
+              comando.CommandText = "SELECT usuario, senha FROM tbl_usuarios WHERE usuario =  '" + textBoxUSUARIO.Text + "' AND senha = '" + textBoxSENHA.Text + "';";   
+
+                MySqlDataReader resultado = comando.ExecuteReader();
+                if (resultado.Read())
+                {
+                    MessageBox.Show("Usuário e senha corretos!"); 
+                }
+                else
+                {
+                    MessageBox.Show("Usuário e/ou senha incorretos!");
+                }                                                                  
+
+            }
+
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+
+
+        private void buttonCADASTRAR_Click(object sender, EventArgs e)
+        {
+           
+            comando.CommandText = "INSERT INTO tbl_usuarios(usuario, senha) VALUES ('" + textBoxUSUARIO.Text  + " ', '" + textBoxSENHA.Text + "');";
+            try
+            {
+                conexao.Open();
+                comando.ExecuteNonQuery();
+                MessageBox.Show("Usuario cadastrado com sucesso!");
+            }
+            catch (Exception erro)
+            {
+                //MessageBox.Show(erro.Message);
+                MessageBox.Show("Erro ao cadastrar novo usuário. Fale com o administrador do sistema");
+            }
+            finally
+            {
+                conexao.Close();   
+            }
+        }
+    }
+}
